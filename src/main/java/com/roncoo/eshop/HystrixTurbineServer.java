8805af107321fec15a570e6520d8db4c30dbea69
WUZe514 <18260624577@163.com>
2021-07-30 18:50:18 +0800

    terbine修改

--- src/main/java/com/roncoo/eshop/HystrixTurbineServer.java
+++ src/main/java/com/roncoo/eshop/HystrixTurbineServer.java
@@ -2,10 +2,12 @@ package com.roncoo.eshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableTurbine
+@EnableDiscoveryClient
 public class HystrixTurbineServer {
 
     public static void main(String[] args) {
